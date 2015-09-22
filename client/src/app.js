ca21ef19752862c072093f78fe6f0c2e52a0ed62
Joel Kelly <joel.kelly@me.com>
2015-09-22 11:12:04 -0700

    Cleanup and simplify routing

--- client/src/app.js
+++ client/src/app.js
@@ -2,10 +2,10 @@ export class App {
   configureRouter(config, router){
     config.title = 'Level Up';
     config.map([
-      { route: ['','welcome'],  name: 'welcome',      moduleId: 'welcome',      nav: true, title:'Welcome' },
-      { route: 'users',         name: 'users',        moduleId: 'users',        nav: true, title:'Github Users' },
-      { route: 'litmus',        name: 'litmus',       moduleId: 'litmus',       nav: true, title:'Style Test' },
-      { route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title:'Child Router' }
+      { route: ['','litmus'],  name: 'litmus',       moduleId: 'litmus',       nav: true, title:'Style Test' }
+      // { route: 'users',         name: 'users',        moduleId: 'users',        nav: true, title:'Github Users' },
+      // { route: 'litmus',        name: 'litmus',       moduleId: 'litmus',       nav: true, title:'Style Test' },
+      // { route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title:'Child Router' }
     ]);
 
     this.router = router;
--- client/src/child-router/child-router.js
+++ client/src/child-router/child-router.js
@@ -5,8 +5,8 @@ export class ChildRouter{
     config.map([
       { route: ['','welcome'],  name: 'welcome',      moduleId: 'welcome',      nav: true, title:'Welcome' },
       { route: 'users',         name: 'users',        moduleId: 'users',        nav: true, title:'Github Users' },
-      { route: 'litmus',        name: 'litmus',       moduleId: 'litmus',       nav: true, title:'Style Test' },
-      { route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title:'Child Router' }
+      // { route: 'litmus',        name: 'litmus',       moduleId: 'litmus',       nav: true, title:'Style Test' },
+      // { route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title:'Child Router' }
     ]);
 
     this.router = router;
