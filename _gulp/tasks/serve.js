7d271cf7df54eb50e6d19bf9896736bb806d09fa
Joel Kelly <joel.kelly@me.com>
2015-09-23 12:59:58 -0700

    Fixing Gulp Task for Serving
    No more multitude of windows.

--- _gulp/tasks/serve.js
+++ _gulp/tasks/serve.js
@@ -1,16 +1,22 @@
 var gulp        = require('gulp');
-var nodemon     = require('gulp-nodemon');
+var exec        = require('child_process').exec;
+// var nodemon     = require('gulp-nodemon');
 
 gulp.task('serve', function (cb) {
-    var started = false;
-    return nodemon({
-        script: 'server/app.js'
-    }).on('start', function () {
-        // to avoid nodemon being started multiple times
-        // thanks @matthisk
-        if (!started) {
-            cb();
-            started = true;
-        }
-    });
+    exec('node server/app.js', function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    cb(err);
+  });
+    // var started = false;
+    // return nodemon({
+    //     script: 'server/app.js'
+    // }).on('start', function () {
+    //     // to avoid nodemon being started multiple times
+    //     // thanks @matthisk
+    //     if (!started) {
+    //         cb();
+    //         started = true;
+    //     }
+    // });
 });
