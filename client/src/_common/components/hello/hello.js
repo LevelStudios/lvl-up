aef5458718e719d13737e4a306e2d0af56048766
Joel Kelly <joel.kelly@me.com>
2015-09-25 13:38:38 -0700

    Adjusting Hello Example

--- client/src/_common/components/hello/hello.js
+++ client/src/_common/components/hello/hello.js
@@ -3,18 +3,24 @@ import {customElement, inject, bindable, noView} from 'aurelia-framework';
 
 @noView()
 @inject(Element)
-@bindable('foo')
+
 export class HelloCustomElement {
+    @bindable firstname
+    @bindable lastname
+    @bindable jobtitle
 
     constructor(element) {this.element = element}
-    render() {React.render(<HelloTest foo={this.foo} />, this.element)}
+    render() {React.render(<HelloTest firstname={this.firstname} lastname={this.lastname} jobtitle={this.jobtitle}/>, this.element)}
     bind() {this.render()}
-    fooChanged() {this.render()}
+    firstnameChanged() {this.render()}
+    lastnameChanged() {this.render()}
+    jobtitleChanged() {this.render()}
+    attached(){this.render()}
 
 }
 
 var HelloTest = React.createClass({
-    render: function () {
-        return (<h1>{this.props.foo}</h1>);
+    render() {
+        return (<div><h2>{this.props.firstname} {this.props.lastname} </h2> <h3> {this.props.jobtitle}</h3></div>);
     }
 });
