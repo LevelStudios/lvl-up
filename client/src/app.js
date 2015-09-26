1890f0ed9cea807d0e38eb5a1443acac488ad65b
Joel Kelly <joel.kelly@me.com>
2015-09-25 17:38:50 -0700

    Adjusting Navbar: WIP

--- client/src/app.js
+++ client/src/app.js
@@ -2,7 +2,8 @@ export class App {
   configureRouter(config, router){
     config.title = 'Level Up';
     config.map([
-      { route: ['','litmus'],  name: 'litmus',       moduleId: 'litmus',       nav: true, title:'Style Test' },
+      { route: ['litmus'],  name: 'litmus',       moduleId: 'litmus',       nav: true,          title:'Style Test' },
+      { route: [''],        name: 'litmus',       moduleId: 'litmus',       nav: true,          title:'Level Up',   classname:true},
       { route: 'suggestions',         name: 'suggestions',        moduleId: 'suggestions',        nav: true, title:'Suggestions' }
       // { route: 'litmus',        name: 'litmus',       moduleId: 'litmus',       nav: true, title:'Style Test' },
       // { route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title:'Child Router' }
